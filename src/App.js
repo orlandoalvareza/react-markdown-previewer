28a267ee2c4c6d0e967cc9e952885c91b05019b8
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-09-23 23:14:55 -0400

    added basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+
+import './index.css';
+
 function App() {
   return (
     <div className="App">
-      
+      <div className='editor-container'>
+        <div className="tool-bar">
+          <h2 className="window-name">Editor</h2>
+          <button className="window-action">Button</button>
+        </div>
+        <textarea id="editor"></textarea>
+      </div>
+      <div className='preview-container'>
+        <div className="tool-bar">
+          <h2 className="window-name">Previewer</h2>
+          <button className="window-action">Button</button>
+        </div>
+        <div id="preview"></div>
+      </div>
     </div>
   );
 }
