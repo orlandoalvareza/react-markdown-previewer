b76b17e949d80c22d8ec773a3c70bf210035c43a
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-09-23 23:24:35 -0400

    added states handler functions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
+import { useState } from 'react';
 
 import './index.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const [isEditorVisible, setIsEditorVisible] = useState(true);
+  const [isPreviewVisible, setIsPreviewVisible] = useState(true);
+
+  const textChangeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  const editorWindowSizeHandler = () => {
+    setIsEditorVisible(isEditorVisible);
+    setIsPreviewVisible(!isPreviewVisible);
+  };
+
+  const previewWindowSizeHandler = () => {
+    setIsEditorVisible(!isEditorVisible);
+    setIsPreviewVisible(isPreviewVisible);
+  };
+
   return (
     <div className="App">
       <div className='editor-container'>
         <div className="tool-bar">
           <h2 className="window-name">Editor</h2>
-          <button className="window-action">Button</button>
+          <button className="window-action" onClick={editorWindowSizeHandler}>Button</button>
         </div>
-        <textarea id="editor"></textarea>
+        <textarea id="editor" onChange={textChangeHandler}>{text}</textarea>
       </div>
       <div className='preview-container'>
         <div className="tool-bar">
           <h2 className="window-name">Previewer</h2>
-          <button className="window-action">Button</button>
+          <button className="window-action" onClick={previewWindowSizeHandler}>Button</button>
         </div>
         <div id="preview"></div>
       </div>
